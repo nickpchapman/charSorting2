8240ae6041058966878beb7fb89f4778f882b888
Nick Chapman <nickpchapman@gmail.com>
2018-01-24 20:41:43 -0800

    Implement getSortedString and fetchStr methods

--- public/app.js
+++ public/app.js
@@ -1,17 +1,15 @@
 const submitButton = document.getElementById('submit-button');
 const string = document.getElementById('string-input');
 
-submitButton.addEventListener('click', function() {
+submitButton.addEventListener('click', () => {
   getSortedString(string.value);
   string.value = '';
 });
 
-const getSortedString = function(str) {
-  console.log(str);
-
-  let sorted = fetchStr(str)
-
-  updateTable(sorted);
-
+const getSortedString = (str) => {
+  fetchStr(str, (err, sortedStr) => {
+    if (err) console.log(err);
+    updateTable(sortedStr);
+  })
 };
 
--- public/utilities.js
+++ public/utilities.js
@@ -1,11 +1,17 @@
-const fetchStr = (unsortedStr) => {
-  console.log('fetching')
-  return 'thisIsASortedString'
+const fetchStr = (unsortedStr, callback) => {
+  const url = 'http://localhost:8080/sort';
+  const data = {str: unsortedStr};
+
+  axios.post(url, data)
+  .then(function (response) {
+    callback(null, response.data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 }
 
 const updateTable = (sortedStr) => {
   console.log('table updated', sortedStr)
 }
 
-
-  //populate return table
